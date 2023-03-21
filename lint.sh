708a9151052e86cf10dfba7efcaadccd5c76efd7
Christian Hettlage <hettlage@saao.ac.za>
2023-03-21 23:05:04 +0200

    Update the script for linting

--- lint.sh
+++ lint.sh
@@ -6,4 +6,4 @@ python -m venv .venv
 source ./.venv/bin/activate
 which python
 python -m pip install flake8
-flake8 calculator/ mytests/
+flake8 --format=pylint calculator/ --exit-zero mytests/ > flake8-out.txt
